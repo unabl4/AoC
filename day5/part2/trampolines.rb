89c2855dc6c2acd09f47174fe01045eb1e26c7d1
Dmitri <unabl4@gmail.com>
2017-12-28 23:53:06 +0200

    Day 5: A Maze of Twisty Trampolines, All Alike

---
+++ day5/part2/trampolines.rb
@@ -0,0 +1,18 @@
+# returns the count of escape jumps
+def jump(list)
+  n = list.size
+  i = 0 # initial
+  c = 0 # jump counter
+  
+  loop do
+    p = i # previous
+    i += list[i]
+    list[p] += (list[p] >= 3) ? -1 : 1
+    c += 1 # +1 step
+    
+    # check if outside
+    break if i > n-1 || i < 0
+  end
+  
+  c # return the number of jumps
+end
